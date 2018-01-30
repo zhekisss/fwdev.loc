cedc81649d41efae3ec6effbf9d664458594b38a
zhekisss <ya.zhekisss@yandex.ru>
2018-01-30 15:06:01 +0500

    DB

---
+++ Vendor/libs/timer.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Класс для измерения времени выполнения скрипта или операций
+ */
+class Timer
+{
+    /**
+     * @var float время начала выполнения скрипта
+     */
+    private static $start = .0;
+
+    /**
+     * Начало выполнения
+     */
+    static function start()
+    {
+        self::$start = microtime(true);
+    }
+
+    /**
+     * Разница между текущей меткой времени и меткой self::$start
+     * @return float
+     */
+    static function finish()
+    {
+        return microtime(true) - self::$start;
+    }
+}
