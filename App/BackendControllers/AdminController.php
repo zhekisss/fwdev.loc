7a02e892fb0545aa97a9254606b1d2e41975c7b2
zhekisss <ya.zhekisss@yandex.ru>
2018-02-15 08:21:03 +0500

    Реализация Admin Backend

--- App/BackendControllers/AdminController.php
+++ App/BackendControllers/AdminController.php
@@ -7,4 +7,10 @@ use \Vendor\Core\Base\Controller;
 class AdminController extends Controller
 {
     
+    public function __construct($route)
+    {
+        // $this->route = $route;
+
+       parent::__construct($route);
+    }
 }
