0747eab401ff2eeca000615f217ce6a48adf7be0
zhekisss <ya.zhekisss@yandex.ru>
2018-02-23 07:59:54 +0500

    MainController extends AppController

--- App/Controllers/MainController.php
+++ App/Controllers/MainController.php
@@ -7,94 +7,84 @@ use Vendor\Core\Registry;
 
 class MainController extends AppController
 {
-
+    
     public $model;
-
+    
     public function __construct($route)
     {
         $this->model = new Main();
         \R::dispense('page');
         parent::__construct($route);
-        // $this->reg->menu = 'Vendor\\Widgets\\Menu\\Menu';
     }
-
+    
     public $cache;
-
+    
     public function indexAction()
     {
-        
-        
-        
-        // $posts = $this->reg->cache->get('posts');
-        // if (!$posts) {
-        $posts = \R::findAll('page', 'LIMIT 2');
-
-        $postsArr = $this->bean2Arr($posts);
-            
-            // $this->reg->cache->set('posts', $posts);
-
         $menuOptions = [
-            'container' => 'div',
-            'className' => 'main-menu'
+        'container' => 'div',
+        'className' => 'main-menu'
         ];
-
-        $menu = (string) $this->reg->get('menu', $menuOptions);
         
-        // }
+        $cacheMenu = $this->reg->get('cache');
+        $menu =  $cacheMenu->get('menu');
+        $menu = (string) $this->reg->get('menu', $menuOptions);
+
+        $cachePost = $this->reg->get('cache');
+        $posts =  $cachePost->get('posts');
+        if (!$posts || !$menu) {
+            $posts = \R::findAll('page', 'LIMIT 2');
             
-        // $model = new Main;
-        // $posts = $model->findAll();
-
-        // $posts = $model->rb::findAll();
-
-        // $posts2 = $model->findAll();
-        // $posts = $model->findOne('Здесь инки не приставали', 'name');
-        // $posts = $model->findBySql("SELECT * FROM {$model->table} WHERE content LIKE ?",['%пасх%']);
-        // $posts = $model->findLike('пасх', 'content', 'page');
-
+            $postsArr = $this->bean2Arr($posts);
+            
+         
+            $cachePost->set('posts', $posts);
+            
+        }
+        
         $method = $this->methodName(__class__, __FUNCTION__);
         $title = 'MAIN TITLE';
         $this->set(compact('title', 'posts', 'postsArr', 'menu'));
         return true;
     }
-
+    
     public function testAction()
     {
-
+        
         if ($this->is_ajax()) {
-
+            
             \R::dispense('page');
             $post = $this->reg->cache->get('post');
             $post = false;
-
+            
             if (!$post) {
-
-                $post = \R::findOne('page', "id={$_POST['id']}");
-                $postArr = $post->export();
+                
+                $postArr = \R::findOne('page', "id={$_POST['id']}")->export();
+                // $postArr = $post->export();
                 $this->reg->cache->set('post', $post);
-
+                
             }
             $postArr['title'] = '<i>JSON object</i>';
             
             // $postArr['reg'] = $this->reg->getList();
             $postArr = json_encode($postArr);
             $this->set(compact('postArr'));
-
+            
         } else {
-
+            
             $title = 'MAIN TITLE';
             $this->set(compact('title'));
             $this->view = 'test';
-
+            
         };
     }
-
+    
     public function viewAction(Type $var = null)
     {
         $this->view = '';
         echo 'qwerty';
     }
-
+    
     public function bean2Arr($posts)
     {
         $count = 0;
