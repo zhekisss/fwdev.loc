5d3f54c963ab5ef487bb3c3cbbef70f7acca8b5d
zhekisss <ya.zhekisss@yandex.ru>
2018-03-08 13:30:10 +0500

    Авторизация

--- Public/index.php
+++ Public/index.php
@@ -1,7 +1,8 @@
 <?php
-// Seeeion start
+
 session_start();
 date_default_timezone_set('Asia/Yekaterinburg');
+
 // Require composer autoload
 require_once "../vendor/autoload.php";
 
@@ -13,21 +14,6 @@ list($env) = explode('/', $query);
 
 // Проверяем что первое слово равно "admin", если да, то загружаем настройки для админ части сайта
 require_once $env == "admin" ? '../config/back_conf.php' : '../config/config_main.php';
-
-// Сканирует папку с библиотеками функций и подключает их
-// $libs = scandir('../' . CORE . '/libs');
-// foreach ($libs as $key) {
-//     if (is_file('../vendor/framework/libs/' . $key)) {
-//         switch ($key) {
-//             case '.':
-//                 continue;
-//             case '..':
-//                 continue;
-//             default:
-//                 require_once '../vendor/framework/libs/' . $key;
-//         }
-//     }
-// }
 
 // Проверка окружения - "admin" или "user"
 if (ENV !== 'admin') {
