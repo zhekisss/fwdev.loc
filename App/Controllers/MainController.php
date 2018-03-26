c8a096f8f0f0547c44a4e014cab2a9f32a274c43
zhekisss <ya.zhekisss@yandex.ru>
2018-03-26 08:40:41 +0500

    1

--- App/Controllers/MainController.php
+++ App/Controllers/MainController.php
@@ -17,23 +17,23 @@ class MainController extends AppController
 
     public function indexAction()
     {
-        $menuOptions = [];
+        // $menuOptions = [];
 
-        $cacheMenu = $this->reg->get('cache');
+        // $cacheMenu = $this->reg->get('cache');
 
-        $menu = $cacheMenu->get('menu');
-        $menu = (string) $this->reg->get('menu', $menuOptions);
-        $posts = false;
-        $cachePost = $this->reg->get('cache');
+        // $menu = $cacheMenu->get('menu');
+        // $menu = (string) $this->reg->get('menu', $menuOptions);
+        // $posts = false;
+        // $cachePost = $this->reg->get('cache');
         // $posts = $cachePost->get('posts');
 
-        if (!$posts ?? null) {
-            $posts = \R::findAll('page');
+        // if (!$posts ?? null) {
+            // $posts = \R::findAll('page');
 
-            $postsArr = $this->bean2Arr($posts);
+            // $postsArr = $this->bean2Arr($posts);
 
-            $cachePost->set('posts', $posts);
-        }
+            // $cachePost->set('posts', $posts);
+        // }
 
         $page = new \StdClass;
         $page->title = 'MAIN';
