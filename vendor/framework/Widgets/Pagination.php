7a70c08dfd8d4c46877d050057a9ff46490aeaf3
zhekisss <ya.zhekisss@yandex.ru>
2018-09-27 19:54:17 +0500

    Пагинация

---
+++ vendor/framework/Widgets/Pagination.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vendor\Widgets;
+
+class Pagination
+{
+    public $currentPage;
+    public $perpage;
+    public $total;
+    public $countPages;
+    public $uri;
+    
+    public function __construct()
+    {
+        # code...
+    }
+    
+}
